b91d6103af3782bc04491246302d1fc00ccec843
Waqituri <waqi.islamian5@gmail.com>
2024-12-19 10:57:52 +0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Waqar Hussain on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
